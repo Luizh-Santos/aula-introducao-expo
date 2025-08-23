1af37fe624febcb7068548a204d2d511659eac85
LUIZ HENRIQUE ANDRADE SANTOS <andradesantos132019@gmail.com>
2025-08-22 21:17:55 -0300

    Aula style 22/08

---
+++ src/exemplos/ex01/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create(
+    {
+        container: {
+            flex: 1,
+            backgroundColor: '#ff0',
+            alignItems: 'center',
+            justifyContent: 'space-evenly',
+        },
+
+        titulo:{
+            fontSize:25,
+            fontWeight: 'black',
+            color:'#777',
+        },
+    }
+);
+
+export default styles;
